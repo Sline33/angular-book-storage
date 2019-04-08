4cd0be003117f50b2310cd90ef50bc150a27f070
Sline <benjaminmartinez@hotmail.fr>
2019-04-08 18:47:27 +0200

    add firebase in app And create signupForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book-storage';
+
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyBqChTskODxOpTxTz7z90rxVCUlpg_pd1c",
+      authDomain: "book-storage-439e8.firebaseapp.com",
+      databaseURL: "https://book-storage-439e8.firebaseio.com",
+      projectId: "book-storage-439e8",
+      storageBucket: "book-storage-439e8.appspot.com",
+      messagingSenderId: "693358849887"
+    };
+    firebase.initializeApp(config);
+  }
 }
