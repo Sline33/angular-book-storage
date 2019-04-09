04d8f33602bc20c53eef1d1ec8929cfc143470af
Sline <benjaminmartinez@hotmail.fr>
2019-04-09 18:20:35 +0200

    add uplaodFile Function

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -6,6 +6,7 @@ import { Book } from '../models/book.model';
 import * as firebase from 'firebase';
 
 import DataSnapshot = firebase.database.DataSnapshot;
+import { resolve, reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -66,6 +67,27 @@ export class BooksService {
     this.books.splice(bookIndexToRemove, 1);
     this.saveBooks();
     this.emitBooks();
+  }
+
+  uploadFile(file: File) {
+    return new Promise(
+      (resolve, reject) => {
+        const almostUniqueFileName = Date.now().toString();
+        const upload = firebase.storage().ref().child('images/' + almostUniqueFileName + file.name).put(file);
+
+        upload.on(firebase.storage.TaskEvent.STATE_CHANGED),
+        () => {
+          console.log('Chargement ...');
+        },
+        (error) => {
+          console.log('Erreur de chargement ! : ' + error);
+          reject();
+        },
+        () => {
+          resolve(upload.snapshot.downloadURL);
+        }
+      }
+    );
   }
 
 }
