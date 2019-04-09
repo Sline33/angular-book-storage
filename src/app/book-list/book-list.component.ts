4a14c7e97464562d8a113aadc9f586fe8d63dc70
Sline <benjaminmartinez@hotmail.fr>
2019-04-09 13:37:10 +0200

    add function for books view

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { BooksService } from '../services/books.service';
+import { Book } from '../models/book.model';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  bookSubcription: Subscription;
+
+  constructor(
+    private booksService: BooksService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.bookSubcription = this.booksService.booksSubject.subscribe(
+      (book: Book[]) => {
+        this.books = book;
+      }
+    );
+    this.booksService.emitBooks();
+  }
+
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view' + id])
+  }
+
+  ngOnDestroy() {
+    this.bookSubcription.unsubscribe();
   }
 
 }
